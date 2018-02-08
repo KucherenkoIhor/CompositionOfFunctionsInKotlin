cf39a5cb80e148476962d7ef4bca88100d79c5a4
Ihor Kucherenko <kuchernkoigor@gmail.com>
2018-02-08 18:37:19 +0200

    Initial commit

---
+++ src/main/kotlin/com/ik/MyBenchmark.kt
@@ -0,0 +1,72 @@
+package com.ik
+
+import org.openjdk.jmh.annotations.*
+import java.util.concurrent.TimeUnit
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+open class MyBenchmark {
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    fun firstCase(): Double {
+
+        val prices = listOf(3.4, 5.6, 5.6, 3.4, 3.4, 3.4, 5.6, 5.6, 3.4, 3.4)
+
+        return prices
+                .map { it - it * 0.9 }
+                .map { it + 0.3 }
+                .map { it - 0.1 }
+                .sum()
+    }
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    fun secondCase(): Double {
+
+        val prices = listOf(3.4, 5.6, 5.6, 3.4, 3.4, 3.4, 5.6, 5.6, 3.4, 3.4)
+
+        return prices
+                .map(::discount)
+                .map(::tax)
+                .map(::aid)
+                .sum()
+    }
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    fun thirdCase(): Double {
+
+        val prices = listOf(3.4, 5.6, 5.6, 3.4, 3.4, 3.4, 5.6, 5.6, 3.4, 3.4)
+
+        return prices
+                .map(::aid + ::tax + ::discount)
+                .sum()
+    }
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    fun fourthCase(): Double {
+
+        val prices = listOf(3.4, 5.6, 5.6, 3.4, 3.4, 3.4, 5.6, 5.6, 3.4, 3.4)
+
+        return prices
+                .map(compose(::aid, ::tax, ::discount))
+                .sum()
+    }
+
+    fun <R> compose(vararg funs: (R) -> R): (R) -> R = { x: R ->
+        funs.reduceRight { acc, function -> { acc(function(it)) } }(x)
+    }
+
+    operator fun <P1, R1, R2> ((R1) -> R2).plus(f: (P1) -> R1): (P1) -> R2 {
+        return { p1: P1 -> this(f(p1)) }
+    }
+
+    fun discount(price: Double) = price - price * 0.9
+
+    fun tax(price: Double) = price + 0.3
+
+    fun aid(price: Double) = price - 0.1
+
+}
